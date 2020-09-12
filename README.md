22194fee02a05ce8faff0b4c5a1480365d1b76d2
Fikri Ali Fata <me.alifata@gmail.com>
2020-09-12 13:44:52 +0700

    1st upload

---
+++ README.md
@@ -0,0 +1,5 @@
+on sale domain alifata.com and bisnisplay.com
+
+contact
+Phone/WhatsApp : +62 858 7520 4463
+Email : me.alifata@gmail.com
---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>On Sale Domain alifata.com</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <p>Domain ini sedang dijual hubungi pemilik untuk miliki domain ini segera
+          <br/>Beri tawaran harga menarik.
+          <br/>
+          <br/>Kontak :
+          <br/>Phone/WhastApp: +62 858 7520 4463
+          <br/>Email: me.alifata@gmail.com
+        </p>
+        <br/><br/>
+        <p>This domain is being sold, contact the elector to own this domain immediately
+          <br/>Give an attractive price offer.
+          <br/>
+          <br/>Contact:
+          <br/>Phone / WhastApp: +62 858 7520 4463
+          <br/>Email: me.alifata@gmail.com
+        </p>
+      </main>
+
+      <footer className={styles.footer}>
+        <p>on sale alifata.com +62 858 7520 4463</p>
+      </footer>
+    </div>
+  )
+}
